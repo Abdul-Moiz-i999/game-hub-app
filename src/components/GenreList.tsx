547f007287fa17bf02636deee7c27eccd130203f
Abdul-Moiz-i999 <aliblkt@gmail.com>
2023-10-29 22:00:34 +0500

    Add Genres, their Images and other refactoring like columns size etc

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-check";
 
 const GenreList = () => {
   const { data: genres } = useGenres();
   return (
-    <ul>
+    <List>
       {genres.map((d) => (
-        <li key={d.id}>{d.name}</li>
+        <ListItem key={d.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(d.image_background)}
+            />
+            <Text>{d.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
