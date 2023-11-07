7e00a828fd7f38d01897f74cc2b3530b531eedd8
Abdul-Moiz-i999 <aliblkt@gmail.com>
2023-11-07 19:12:54 +0500

    Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,16 +9,16 @@ function GameCard({ game }: { game: Game }) {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
         {/* {game.parent_platforms.map(({ platform }) => (
           <Text>{platform.name}</Text>
         ))} */}
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
