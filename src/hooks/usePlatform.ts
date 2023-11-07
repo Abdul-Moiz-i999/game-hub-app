8107bb11ddcc9b1061bb13684adff9833b45f23e
Abdul-Moiz-i999 <aliblkt@gmail.com>
2023-11-07 20:33:11 +0500

    Ship static data (genres and platform) with the app

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,6 +1,8 @@
 import useData from "./useData";
 import { Platform } from "./useGames";
+import platform from "../data/platform";
 
-const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+// const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+const usePlatform = () => ({ data: platform, isLoading: false, error: null });
 
 export default usePlatform;
