042fb37647a21c45e0c3672939b799d0c9aa246f
Abdul-Moiz-i999 <67798125+Abdul-Moiz-i999@users.noreply.github.com>
2023-11-09 02:35:51 +0500

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios, { CanceledError } from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: ""
+    key: "3d93cdc9b0f84b6ea89c0af002ed048c"
   },
 });
 
